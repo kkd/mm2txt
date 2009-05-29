ecc8aad8d9967f6fec4fd6ec734b221d9b6a549f
Takeshi Kakeda <takeshi@giantech.jp>
2009-05-29 12:04:32 +0900

    refactored for extracting classes.

--- mm2txt.rb
+++ mm2txt.rb
@@ -1,37 +1,106 @@
 #!/usr/bin/env ruby
 
-def format_as_tabbed_text(line)
-  text = ''
-  cols = line.split("\t")
-  strings = cols[cols.count-1]
-  count_of_tabs = cols.count{|c| c==""} 
+class Formatter
+  TOP = 0
+  SECOND = 1
+  THIRD = 2
 
-  case count_of_tabs
-  when 0
-    text << "=" * (strings.size * 1.5) << "\n"
-    text << strings
-    text << "=" * (strings.size * 1.5) << "\n"
+  protected
+
+  def format_as_top_level(content)
+    content
+  end
+  def format_as_second_level(content)
+    content
+  end
+  def format_as_third_level(content)
+    content
+  end
+  def format_as_lower_level(content)
+    content
+  end
+
+  def split_by_tab(line)
+    line.split("\t")
+  end
+
+  def extract_content(cols)
+    cols[cols.count-1]
+  end
+
+  def indent_level(cols)
+    cols.count{|c| c==""} 
+  end
+
+
+end
+
+class ReSTFormatter < Formatter
+
+
+  def format(line)
+    cols = split_by_tab(line)
+    content = extract_content(cols)
+    level = indent_level(cols)
+    case level
+    when TOP
+      return format_as_top_level(content)
+    when SECOND
+      return format_as_second_level(content)
+    when THIRD
+      return format_as_third_level(content)
+    else
+      return format_as_lower_level(content, level)
+    end
+  end
+
+  protected
+
+  def format_as_top_level(content)
+    text = ''
+    text << "=" * (content.size * 1.5) << "\n"
+    text << content
+    text << "=" * (content.size * 1.5) << "\n"
     text << "\n\n"
-  when 1
-    text << strings
-    text << "=" * (strings.size * 1.5)
+  end
+
+  def format_as_second_level(content)
+    text = ''
+    text << content
+    text << "=" * (content.size * 1.5)
     text << "\n\n"
-  when 2
-    text << strings
-    text << "-" * (strings.size * 1.5)
+  end
+
+  def format_as_third_level(content)
+    text = ''
+    text << content
+    text << "-" * (content.size * 1.5)
     text << "\n\n"
-  else
+  end
+
+  def format_as_lower_level(content, indent_level)
+    text = ''
     text << " "
-    text << "  " * (count_of_tabs - 2)
+    text << "  " * (indent_level - 2)
     text << "* "
-    text << strings
+    text << content
     text << "\n"
   end
-  return text
+
+
 end
+
+class RedmineFormatter < Formatter
+end
+
+def format_as_tabbed_text(line)
+  formatter = ReSTFormatter.new
+  formatter.format(line)
+end
+
 file = ARGV[0]
 if file.nil?
-  puts "Usage: jude2txt.rb textfile"
+  puts "Usage: mm2txt.rb textfile"
   exit(1)
 end
 
